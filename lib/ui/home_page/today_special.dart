cb4337c7d741726f1822b8c27bd4c16bfa125ac8
Albert Christian <albert.christian92@gmail.com>
2022-03-08 20:35:03 +0800

    Latest updated

--- lib/ui/home_page/today_special.dart
+++ lib/ui/home_page/today_special.dart
@@ -4,6 +4,32 @@ import 'package:provider/provider.dart';
 import 'package:dicoding_rest_api/widgets/error_information.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
+const List<StaggeredTile> _staggeredTiles = <StaggeredTile>[
+  StaggeredTile.count(2, 2),
+  StaggeredTile.count(2, 1),
+  StaggeredTile.count(2, 1),
+  // StaggeredTile.count(1, 1),
+  // StaggeredTile.count(2, 2),
+  // StaggeredTile.count(1, 2),
+  // StaggeredTile.count(1, 1),
+  // StaggeredTile.count(3, 1),
+  // StaggeredTile.count(1, 1),
+  // StaggeredTile.count(4, 1),
+];
+
+const List<Widget> _tiles = <Widget>[
+  _Example09Tile(Colors.green, Icons.widgets),
+  _Example09Tile(Colors.lightBlue, Icons.wifi),
+  _Example09Tile(Colors.amber, Icons.panorama_wide_angle),
+  // _Example09Tile(Colors.brown, Icons.map),
+  // _Example09Tile(Colors.deepOrange, Icons.send),
+  // _Example09Tile(Colors.indigo, Icons.airline_seat_flat),
+  // _Example09Tile(Colors.red, Icons.bluetooth),
+  // _Example09Tile(Colors.pink, Icons.battery_alert),
+  // _Example09Tile(Colors.purple, Icons.desktop_windows),
+  // _Example09Tile(Colors.blue, Icons.radio),
+];
+
 class TodaySpecial extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
