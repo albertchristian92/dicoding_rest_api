7e7f69838f8d30311439eae4d369a9a9cfaa3428
Albert Christian <albert.christian92@gmail.com>
2022-03-08 20:37:58 +0800

    Latest updated

--- lib/ui/home_page/today_special.dart
+++ lib/ui/home_page/today_special.dart
@@ -4,31 +4,6 @@ import 'package:provider/provider.dart';
 import 'package:dicoding_rest_api/widgets/error_information.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-const List<StaggeredTile> _staggeredTiles = <StaggeredTile>[
-  StaggeredTile.count(2, 2),
-  StaggeredTile.count(2, 1),
-  StaggeredTile.count(2, 1),
-  // StaggeredTile.count(1, 1),
-  // StaggeredTile.count(2, 2),
-  // StaggeredTile.count(1, 2),
-  // StaggeredTile.count(1, 1),
-  // StaggeredTile.count(3, 1),
-  // StaggeredTile.count(1, 1),
-  // StaggeredTile.count(4, 1),
-];
-
-const List<Widget> _tiles = <Widget>[
-  _Example09Tile(Colors.green, Icons.widgets),
-  _Example09Tile(Colors.lightBlue, Icons.wifi),
-  _Example09Tile(Colors.amber, Icons.panorama_wide_angle),
-  // _Example09Tile(Colors.brown, Icons.map),
-  // _Example09Tile(Colors.deepOrange, Icons.send),
-  // _Example09Tile(Colors.indigo, Icons.airline_seat_flat),
-  // _Example09Tile(Colors.red, Icons.bluetooth),
-  // _Example09Tile(Colors.pink, Icons.battery_alert),
-  // _Example09Tile(Colors.purple, Icons.desktop_windows),
-  // _Example09Tile(Colors.blue, Icons.radio),
-];
 
 class TodaySpecial extends StatelessWidget{
   @override
@@ -36,7 +11,8 @@ class TodaySpecial extends StatelessWidget{
     return Consumer<RestaurantProvider>(builder: (context, state, _) {
       if (state.state == ResultState.Loading) {
         return Center(child: CircularProgressIndicator());
-      } else if (state.state == ResultState.HasData) {
+      } else if (state.state == ResultState.HasData)
+      {
         const List<StaggeredTile> _staggeredTiles = <StaggeredTile>[
           StaggeredTile.count(2, 2),
           StaggeredTile.count(2, 1),
@@ -60,7 +36,8 @@ class TodaySpecial extends StatelessWidget{
         return Center(child: Text(state.message));
       } else if (state.state == ResultState.Error) {
         return ErrorInformation();
-      } else {
+      } else
+        {
         return Text('');
       }
       return ErrorInformation();
