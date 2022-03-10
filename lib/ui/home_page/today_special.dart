1b3cef609846bebf3e8667c4c33d377f367ad87d
Albert Christian <albert.christian92@gmail.com>
2022-03-10 19:40:26 +0800

    Latest updated

--- lib/ui/home_page/today_special.dart
+++ lib/ui/home_page/today_special.dart
@@ -3,6 +3,8 @@ import 'package:dicoding_rest_api/provider/restaurant_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:dicoding_rest_api/widgets/error_information.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:dicoding_rest_api/common/common.dart';
+import 'package:flutter/widgets.dart';
 
 
 class TodaySpecial extends StatelessWidget{
@@ -13,25 +15,38 @@ class TodaySpecial extends StatelessWidget{
         return Center(child: CircularProgressIndicator());
       } else if (state.state == ResultState.HasData)
       {
-        const List<StaggeredTile> _staggeredTiles = <StaggeredTile>[
-          StaggeredTile.count(2, 2),
-          StaggeredTile.count(2, 1),
-          StaggeredTile.count(2, 1),
-        ];
-        const List<Widget> _tiles = <Widget>[
-          _Example09Tile(Colors.green, Icons.widgets),
-          _Example09Tile(Colors.lightBlue, Icons.wifi),
-          _Example09Tile(Colors.amber, Icons.panorama_wide_angle),
-        ];
-        return StaggeredGridView.count(
-          scrollDirection: Axis.horizontal,
-          children: _tiles,
-          crossAxisCount: 4,
-          staggeredTiles: _staggeredTiles,
-          mainAxisSpacing: 4.0,
-          crossAxisSpacing: 4.0,
-          );
-
+        StaggeredGrid.count(
+            crossAxisCount: 4,
+            mainAxisSpacing: 4,
+            crossAxisSpacing: 4,
+            children: const [
+              StaggeredGridTile.count(
+                crossAxisCellCount: 2,
+                mainAxisCellCount: 2,
+                child: Tile(index: 0),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 2,
+                mainAxisCellCount: 1,
+                child: Tile(index: 1),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Tile(index: 2),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 1,
+                mainAxisCellCount: 1,
+                child: Tile(index: 3),
+              ),
+              StaggeredGridTile.count(
+                crossAxisCellCount: 4,
+                mainAxisCellCount: 2,
+                child: Tile(index: 4),
+              ),
+            ],
+        );
       } else if (state.state == ResultState.NoData) {
         return Center(child: Text(state.message));
       } else if (state.state == ResultState.Error) {
@@ -42,30 +57,5 @@ class TodaySpecial extends StatelessWidget{
       }
       return ErrorInformation();
     });
-  }
-}
-class _Example09Tile extends StatelessWidget {
-  const _Example09Tile(this.backgroundColor, this.iconData);
-
-  final Color backgroundColor;
-  final IconData iconData;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: backgroundColor,
-      child: InkWell(
-        onTap: () {},
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(4),
-            child: Icon(
-              iconData,
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-    );
   }
 }
